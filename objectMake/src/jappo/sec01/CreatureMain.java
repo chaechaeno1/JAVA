7245d3b0d2d721d14ba5aed70dfe1e3b59b5fa0a
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-08-03 09:43:47 +0900

    Update CreatureMain.java

--- objectMake/src/jappo/sec01/CreatureMain.java
+++ objectMake/src/jappo/sec01/CreatureMain.java
@@ -6,12 +6,14 @@ public class CreatureMain {
 		
 		//방법1. 변수로 호출
 		Turtle turtle = new Turtle();
+		Jan jan = new Jan();
 		
 		//방법2. Animal 타입 변수로 자동 타입 변환
 		Animal animal = null;
 		animal = new Pigeon();
 		
 		turtle.bite();
+		jan.coding();
 	
 
 	}
